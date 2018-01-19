10e4ca56a94ac04655dabcb95470a199cd4cbba5
andreobueno <andre.obueno@gmail.com>
2018-01-18 19:49:31 -0800

    Initial commit

---
+++ meanauthapp_part_3/routes/users.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+// Register
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if(err){
+      res.json({success: false, msg:'Failed to register user'});
+    } else {
+      res.json({success: true, msg:'User registered'});
+    }
+  });
+});
+
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+  res.send('AUTHENTICATE');
+});
+
+// Profile
+router.get('/profile', (req, res, next) => {
+  res.send('PROFILE');
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jst = require('jsonwebtoken');
+
+
+const User = require('../models/user');
+
+
+// Register
+router.post('/register', (req, res, next) => {
+	let newUser = new User({
+		name: req.body.name,
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password
+	});
+
+	User.addUser(newUser, (err, user) => {
+		if(err){
+			res.json({success: false, msg: 'Failed to register users'});
+		} else {
+			res.json({success: true, msg: 'User registered'});
+		}
+
+	});
+});
+
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+	res.send('AUTHENTICATE');
+});
+
+// Profile
+router.get('/profile', (req, res, next) => {
+	res.send('PROFILE');
+});
+
+module.exports = router;
