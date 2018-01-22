309018ae2dbc605f001b80f933bdc5a29798dfe6
andreobueno <andre.obueno@gmail.com>
2018-01-22 01:26:44 -0800

    linked mongodb from mlab

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-	database: 'mongodb://localhost:27017/meanapp',
+	//database: 'mongodb://localhost:27017/meanapp',
+	database: 'mongodb://andre.obueno:Upabeelo123_@ds111478.mlab.com:11478/andre_meanapp',
 	secret: 'mysecret'
 }
